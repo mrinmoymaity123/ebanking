a44d9c6f55827137bec724aff1bcbc24868dbf4d
mrinmoymaity123 <50178237+mrinmoymaity123@users.noreply.github.com>
2019-05-01 22:14:41 +0530

    banking

---
+++ welcome.php
@@ -0,0 +1,185 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>E-Banking :: Login</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<!-- Custom Theme files -->
+<link href="css/bootstrap.css" type="text/css" rel="stylesheet" media="all">
+<link href="css/style.css" type="text/css" rel="stylesheet" media="all">
+<link href="css/font-awesome.css" rel="stylesheet">   <!-- font-awesome icons -->
+<link rel="stylesheet" href="css/lightbox.css">
+<!-- //Custom Theme files -->  
+<!-- js --> 
+	<script src="js/jquery-2.2.3.min.js"></script>
+<!-- web-fonts -->
+<link href="//fonts.googleapis.com/css?family=Secular+One" rel="stylesheet">
+<link href="//fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i" rel="stylesheet">
+<link href="//fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+<!-- //web-fonts --> 
+
+</head>
+<body> 
+	<!-- header -->
+	<div class="headerw3-agile"> 
+		<div class="header-w3mdl"><!-- header-two --> 
+			<div class="container"> 
+				<div class="agileits-logo navbar-left">
+					<h1><a href="index.html"><img src="images/e.png" alt="logo"/>Banking</a></h1> 
+				</div> 
+				<div class="agileits-hdright nav navbar-nav">
+					<div class="header-w3top"><!-- header-top --> 
+						<ul class="w3l-nav-top">
+							<li><i class="fa fa-phone"></i><span> +919547756319 </span></li> 
+							<li><a href="mailto:mrinmoymaity118@gmail.com"><i class="fa fa-envelope-o"></i><span>mail@help.com</span></a></li>
+						</ul>
+						<div class="clearfix"> </div> 	 
+					</div>
+					<div class="agile_social_banner">
+						<ul class="agileits_social_list">
+							<li><a href="#" class="w3_agile_facebook"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+							<li><a href="#" class="agile_twitter"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+							<li><a href="#" class="w3_agile_dribble"><i class="fa fa-dribbble" aria-hidden="true"></i></a></li>
+							<li><a href="#" class="w3_agile_vimeo"><i class="fa fa-vimeo" aria-hidden="true"></i></a></li>
+						</ul>
+					</div>  
+
+				</div>
+				<div class="clearfix"> </div> 
+			</div>	
+		</div>	
+	</div>	
+	<!-- //header -->  
+	<!-- banner -->
+	<div class="banner inner-banner">
+		<div class="header-nav"><!-- header-three --> 	
+			<nav class="navbar navbar-default">
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+						<span class="sr-only">Toggle navigation</span>
+						Menu 
+					</button> 
+				</div>
+				<!-- top-nav -->
+				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+					<ul class="nav navbar-nav">
+						<li><a href="index.html">Home</a></li>
+						<li><a href="about.html">About</a></li>    
+						<li><a href="services.html">services</a></li>    
+						<li><a href="gallery.html" >Gallery</a></li>    
+							
+						<li><a href="contact.html">Contact Us</a></li>
+					</ul>  
+					<div class="clearfix"> </div>	
+				</div>
+			</nav>    
+		</div>
+	</div>	
+	<!-- banner -->
+	<!-- gallery -->
+	<div class="w3ls-section gallery">
+		<div class="container">    
+			<div class="container-fluid">
+  <h1>Welcome to your account</h1>
+  <p>The following example will result in a 25%/75% split on small devices and a 50%/50% split on medium (and large) devices. On extra small devices, it will automatically stack (100%).</p><br>
+  
+  <div class="row">
+    <div class="col-sm-3 col-md-6">
+    
+      <img src="https://www.w3schools.com/howto/img_avatar.png" alt="" height="150px" width="150px">
+  
+    </div>
+    <div class="col-sm-9 col-md-6">
+      <h4>Name</h4><br>
+      <h4>Id</h4><br>
+      <h4>Address</h4><br>
+      <h4>Emaill</h4><br>
+      <br><br><br>
+
+      <h2>Available Balance: &nbsp; <span style="color:blue;">Rs. 24,000/-</span></h2><br><br>
+      <button type="button" class="btn"><a href="https://paytm.com/paytmwallet" target="_blank">Add Money</a></button>
+    </div>
+  </div>
+</div>
+		</div>
+	</div>
+	<!-- //gallery -->     
+
+<!--footer-->
+<div class="agile-footer w3ls-section">
+	<div class="container">
+		<div class="col-md-7 list-footer">
+		  <ul class="footer-nav">
+				<li><a  href="index.html">Home</a></li>
+				<li><a  href="about.html">About</a></li>
+				<li><a  href="services.html">Services</a></li>
+				<li><a href="gallery.html">Gallery</a></li>
+                <li><a href="contact.html">Contact Us</a></li>
+                <li><a href="login.html">Login</a></li>
+		  </ul>
+		  <p></p>
+		</div>
+		<div class="col-md-5 agileinfo-sub">
+			<h6>Click the link below to start the subscription service</h6>
+			<a href="#" data-toggle="modal" data-target="#myModal1">subscribe</a>
+		</div>
+		<div class="clearfix"></div>
+     </div>
+</div>	 
+<div class="w3_agile-copyright text-center">
+		<p>© 2018 E-Banking. All rights reserved</p>
+	</div>
+<!--//footer-->	
+<!-- modal-subscribe-->  
+	<div class="modal bnr-modal fade" id="myModal1" tabindex="-1" role="dialog">
+		<div class="modal-dialog" role="document">
+			<div class="modal-content">
+				<div class="modal-header">
+					<img src="images/logo.png" alt="logo"/>
+					<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>						
+				</div> 
+				<div class="modal-body modal-spa">
+					<p>E-Banking's email newsletter provides subscribers with helpful articles on important issues in the banking industry, as well as news about events and more! To sign up for the newsletter, fill the below form.</p>
+					<form class=" wthree-subsribe" action="#" method="post"> 
+						<input type="text" name="name" placeholder="Your Name" required="">
+						<input type="email" name="email" placeholder="your Email" required="">
+						<input type="submit" value="SignUp"> 
+						<div class="clearfix"></div>
+					</form>
+				</div> 
+			</div>
+		</div>
+	</div>
+	<!-- //modal-subscribe-->  
+	<script src="js/SmoothScroll.min.js"></script>
+	<!-- smooth-scrolling-of-move-up -->
+	<script type="text/javascript" src="js/move-top.js"></script>
+	<script type="text/javascript" src="js/easing.js"></script>
+	<script type="text/javascript">
+		$(document).ready(function() {
+			/*
+			var defaults = {
+				containerID: 'toTop', // fading element id
+				containerHoverID: 'toTopHover', // fading element hover id
+				scrollSpeed: 1200,
+				easingType: 'linear' 
+			};
+			*/
+			
+			$().UItoTop({ easingType: 'easeOutQuart' });
+			
+		});
+	</script>
+	<!-- //smooth-scrolling-of-move-up -->  	
+	<!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="js/bootstrap.js"></script>
+
+</body>
+</html>
+
+
+
+
